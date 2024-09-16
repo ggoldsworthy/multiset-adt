69a0cb13901332bd11f78da73b5eb8bbc9dc902a
cedricjizmejian <cedric.jizmejian@mail.utoronto.ca>
2024-09-16 12:41:07 -0400

    added count method

--- src/MultiSet.java
+++ src/MultiSet.java
@@ -12,6 +12,8 @@ public abstract class MultiSet {
 
     abstract void remove(Integer item);
 
+    abstract Integer count(Integer item);
+
     // TODO finish adding abstract methods to fully describe what it means to be a MultiSet.
 
     abstract int remove(Integer item);
